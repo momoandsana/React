dec38cd74ab008d65373df6bd686b8b79200da64
hoseok <swiftie52@naver.com>
2024-11-12 12:29:45 +0900

    feat : 11/12 진도까지

--- step02-vite/src/App.jsx
+++ step02-vite/src/App.jsx
@@ -5,10 +5,13 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  let text="text test"
 
   return (
+    // jsx 문법, div가 아닌 비어 있는 <> 태그도 자주 사용한다
     <>
      <h1> Vite 프로젝트</h1>
+     {text}
     </>
   )
 }
