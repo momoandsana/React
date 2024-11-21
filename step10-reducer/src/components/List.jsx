7040f8cc69aa2827c1dbc493d0c19cf388d849ec
hoseok <swiftie52@naver.com>
2024-11-21 09:39:37 +0900

    fix : todoList->reducer 로 변경

---
+++ step10-reducer/src/components/List.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import "./List.css";
+import TodoItem from './TodoItem';
+
+const List = ({ todos, onUpdate, onDelete }) => { // props 구조 분해
+    const [search, setSearch] = useState("");
+
+    // todos가 배열인지 확인하는 방어 코드 추가
+    if (!Array.isArray(todos)) {
+        console.error("Error: todos is not an array", todos);
+        return null; // 또는 에러 메시지를 보여줄 수 있습니다.
+    }
+
+    // 검색어를 입력했을 때 검색어를 포함한 todos 정보 조회
+    const getFilterData=()=>{
+     if(search==="")return todos;
+
+     const searchedTodos
+     =todos.filter((todo)=>todo.content.toLowerCase().includes(search.toLowerCase()));
+     return searchedTodos;
+    }
+
+    const filteredTodos=getFilterData();// lIst 컴포넌트가 리랜더링 될 때마다
+
+    return (
+        <div className="List">
+            <h4>ToDo List 😸</h4>
+            <input
+                placeholder='검색어를 입력해주세요.'
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+
+            <div className="todos_wrapper">
+                {
+               //  todos.map((todo) => (
+                    filteredTodos.map((todo) => (
+                    <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} onDelete={onDelete} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default List;
